bf0a12c191530160576ae32bce881cd9f34362e0
Yash Lohakare <yashlohakare48@outlook.com>
2021-11-15 13:33:49 +0530

    with binary

--- haruka/modules/pahe.py
+++ haruka/modules/pahe.py
@@ -40,7 +40,7 @@ def pahedl(bot: Bot, update: Update):
     profile.set_preference("browser.download.manager.showWhenStarting", False)
     profile.set_preference("browser.download.dir", str(update.effective_user.id))
     profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/x-gzip")
-    driver = webdriver.Firefox(profile, executable_path=os.environ.get('GECKODRIVER_PATH'))
+    driver = webdriver.Firefox(profile, firefox_binary=binary, executable_path=os.environ.get('GECKODRIVER_PATH'))
     driver.get('https://pahe.ph/')
 
     # Finding The SearchBox
