6f6558d7bcf50889bed3cb3965716b78b6f1c3c6
Yash Lohakare <yashlohakare48@outlook.com>
2021-11-25 19:10:52 +0530

    episode still ain't changing

--- haruka/modules/pahems.py
+++ haruka/modules/pahems.py
@@ -210,17 +210,16 @@ def pahesh(bot: Bot, update: Update):
             cLi = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[1]/div[1]/article/div/div[2]/div['+str(x+2)+']/ul/li['+str(y+1)+']')
             print(cLi.text)
             res += cLi.text + '\n'
+            cLi.click()
             driver.execute_script("arguments[0].click();", cLi)
             #driver.execute_script("arguments[0].click();", cLi)
             # /html/body/div[1]/div[2]/div/div[1]/div[1]/article/div/div[2]/div[2]/ul/li[2]
-            try:
-                print(driver.find_element_by_xpath('//li[@class="current"]').text)
-            except:
-                pass
+            print(driver.find_element_by_xpath('//li[@class="current"]').text)
 
             # code
             start = 0
             del cLi
+            print("getting division text")
             nameDiv = driver.find_elements_by_xpath(
                 '/html/body/div[1]/div[2]/div/div[1]/div[1]/article/div/div[2]/div['+str(x+2)+']/div')[y]
             cText = nameDiv.text
@@ -259,8 +258,10 @@ def pahesh(bot: Bot, update: Update):
                     cLi = driver.find_element_by_xpath(
                         '/html/body/div[1]/div[2]/div/div[1]/div[1]/article/div/div[2]/div[' + str(x + 2) + ']/ul/li[' + str(
                                     y + 1) + ']')
+                    cLi.click()
                     driver.execute_script("arguments[0].click();", cLi)
                     driver.execute_script("arguments[0].click();", cLi)
+                    print(driver.find_element_by_xpath('//li[@class="current"]').text)
                     #for o in range(0, 2):
                     print("Finding red button")
                     GoogleDriveLink = WebDriverWait(driver, 100).until(
