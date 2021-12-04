8f873adaaaa27cdbde1186d96f5e64d49e210ed4
Yash Lohakare <yashlohakare48@outlook.com>
2021-12-04 14:20:12 +0530

    Back to the future

--- haruka/modules/pahe.py
+++ haruka/modules/pahe.py
@@ -19,102 +19,6 @@ import re
 import os
 
 
-def getFromInter(link: str):
-    megaLink = ""
-    print("Starting new driver")
-    options = webdriver.FirefoxOptions()
-    options.log.level = "trace"
-    options.add_argument("-remote-debugging-port=9224")
-    options.add_argument("-headless")
-    options.add_argument("-disable-gpu")
-    options.add_argument("-no-sandbox")
-    binary = FirefoxBinary(os.environ.get('FIREFOX_BIN'))
-    tDriver = webdriver.Firefox(firefox_binary=binary, executable_path=os.environ.get('GECKODRIVER_PATH'),
-                                options=options)
-    tDriver.get(link)
-    time.sleep(5)
-    print("Finally here!", tDriver.current_url)
-
-    # Clicking Diasagree for coockies
-    try:
-        WebDriverWait(tDriver, 15).until(
-            EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div/div[2]/div/button[1]'))).click()
-    except:
-        pass
-    # driver.find_element_by_xpath("//button[contains(., 'DISAGREE')]").click()
-    # Clicking I Am Not A Robot Button
-    try:
-        Robot = WebDriverWait(tDriver, 100).until(
-            EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[1]/div/form/div/div[2]/center/img')))
-    except:
-        tDriver.quit()
-        return "NA"
-    Robot.location_once_scrolled_into_view
-    Robot.click()
-    print("Robot Passed")
-
-    # Adding 15 Second Pause For Loading The Page
-    time.sleep(15)
-
-    # Clicking Generate Link Button
-    print("Generating Link")
-    GenerateLink = tDriver.find_element_by_xpath('//*[@id="generater"]')
-    GenerateLink.click()
-
-    # Adding 15 Second Pause For Loading The Page
-    time.sleep(15)
-
-    # Clicking Download To Get Redirected To Spacetica
-    print("Clicking Download button!:/")
-    Down = tDriver.find_element_by_xpath('//img[@id="showlink"]')
-    Down.click()
-
-    # Adding 15 Second Pause For Loading The Page
-    time.sleep(15)
-
-    # ind = -1
-    print("len", len(tDriver.window_handles))
-    '''for i in range(0, len(tDriver.window_handles)):
-        tDriver.switch_to.window(tDriver.window_handles[i])
-        print("checking", tDriver.current_url)
-        if "linegee.net" in str(tDriver.current_url) or "spacetica.com" in str(tDriver.current_url):
-            ind = i
-            print("ind", ind)
-            break'''
-
-    # Switching To The Newly Opened Tab linegee.net
-    window_after = tDriver.window_handles[-1]
-    tDriver.switch_to.window(window_after)
-    print("On new tab")
-
-    # Addin 5 Second Pause To Load The Page Properly
-    time.sleep(3)
-    print(tDriver.title, tDriver.current_url)
-
-    # Clicking Continue Button On Spacetica
-    try:
-        if "Linegee" in tDriver.title:
-            Con = WebDriverWait(tDriver, 60).until(EC.element_to_be_clickable(
-                (By.XPATH, '/html/body/div[2]/section[2]/div/div/div[1]/div/div[1]/div[3]/center/p/a')))
-        else:
-            Con = WebDriverWait(tDriver, 60).until(EC.element_to_be_clickable(
-                (By.XPATH, '/html/body/section/div/div/div/div[3]/a')
-            ))
-    except:
-        print("No Continue Button")
-        return "NA"
-    Con.location_once_scrolled_into_view
-    # Con = driver.find_element_by_xpath('/html/body/div[2]/section[2]/div/div/div[1]/div/div[1]/div[3]/center/p/a')
-    Con.click()
-    print("Clicked Continue")
-    time.sleep(5)
-    tDriver.switch_to.window(tDriver.window_handles[-1])
-    megaLink = tDriver.current_url
-    print(megaLink)
-    tDriver.quit()
-    return megaLink
-
-
 @run_async
 def pahedl(bot: Bot, update: Update):
     msg = update.effective_message
@@ -229,14 +133,80 @@ def pahedl(bot: Bot, update: Update):
 
             # on intercelestial
             time.sleep(5)
-            print("len", len(driver.window_handles))
+            '''print("len", len(driver.window_handles))
             linktc = driver.current_url
             print(linktc)
             driver.quit()
             time.sleep(5)
             mLink = getFromInter(linktc)
             if mLink == "NA":
-                raise Exception('NO MEGA LINK')
+                raise Exception('NO MEGA LINK')'''
+            try:
+                WebDriverWait(driver, 15).until(
+                    EC.element_to_be_clickable(
+                        (By.XPATH, '/html/body/div[1]/div/div/div/div[2]/div/button[1]'))).click()
+            except:
+                pass
+            # driver.find_element_by_xpath("//button[contains(., 'DISAGREE')]").click()
+            # Clicking I Am Not A Robot Button
+            try:
+                Robot = WebDriverWait(driver, 100).until(
+                    EC.element_to_be_clickable(
+                        (By.XPATH, '/html/body/div[2]/div/div[1]/div/form/div/div[2]/center/img')))
+            except:
+                driver.quit()
+                raise Exception("No Mega Link")
+            Robot.location_once_scrolled_into_view
+            Robot.click()
+            print("Robot Passed")
+
+            # Adding 15 Second Pause For Loading The Page
+            time.sleep(15)
+
+            # Clicking Generate Link Button
+            print("Generating Link")
+            GenerateLink = driver.find_element_by_xpath('//*[@id="generater"]')
+            GenerateLink.click()
+
+            # Adding 15 Second Pause For Loading The Page
+            time.sleep(15)
+
+            # Clicking Download To Get Redirected To Spacetica
+            print("Clicking Download button!:/")
+            Down = driver.find_element_by_xpath('//img[@id="showlink"]')
+            Down.click()
+
+            time.sleep(15)
+            print("len", len(driver.window_handles))
+            # Switching To The Newly Opened Tab linegee.net
+            window_after = driver.window_handles[-1]
+            driver.switch_to.window(window_after)
+            print("On new tab")
+
+            time.sleep(3)
+            print(driver.title, driver.current_url)
+
+            # Clicking Continue Button On Spacetica
+            try:
+                if "Linegee" in driver.title:
+                    Con = WebDriverWait(driver, 60).until(EC.element_to_be_clickable(
+                        (By.XPATH, '/html/body/div[2]/section[2]/div/div/div[1]/div/div[1]/div[3]/center/p/a')))
+                else:
+                    Con = WebDriverWait(driver, 60).until(EC.element_to_be_clickable(
+                        (By.XPATH, '/html/body/section/div/div/div/div[3]/a')
+                    ))
+            except:
+                print("No Continue Button")
+                raise Exception("No Mega")
+            Con.location_once_scrolled_into_view
+            # Con = driver.find_element_by_xpath('/html/body/div[2]/section[2]/div/div/div[1]/div/div[1]/div[3]/center/p/a')
+            Con.click()
+            print("Clicked Continue")
+            time.sleep(5)
+            # tDriver.switch_to.window(tDriver.window_handles[-1])
+            mLink = driver.current_url
+            print(mLink)
+            driver.quit()
         except Exception as e:
             try:
                 driver.quit()
